5239d36458d252c2c8a0a03f15f62fcbffa3e9d5
timorlan <119798299+timorlan@users.noreply.github.com>
2024-06-02 18:59:10 +0300

    Update LoginPage.vue

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,63 +1,65 @@
 <template>
   <div class="login-container">
-    <div class="login-form">
-      <h1 class="title">Login</h1>
-      <b-form @submit.prevent="onLogin">
-        <b-form-group
-          id="input-group-Username"
-          label-cols-sm="3"
-          label="Username:"
-          label-for="Username"
-        >
-          <b-form-input
-            id="Username"
-            v-model="$v.form.username.$model"
-            type="text"
-            :state="validateState('username')"
-          ></b-form-input>
-          <b-form-invalid-feedback>
-            Username is required
-          </b-form-invalid-feedback>
-        </b-form-group>
+    <div class="image-section"></div>
+    <div class="login-form-section">
+      <div class="login-form">
+        <h1 class="title">Login</h1>
+        <b-form @submit.prevent="onLogin">
+          <b-form-group
+            id="input-group-Username"
+            label-cols-sm="3"
+            label="Username:"
+            label-for="Username"
+          >
+            <b-form-input
+              id="Username"
+              v-model="$v.form.username.$model"
+              type="text"
+              :state="validateState('username')"
+            ></b-form-input>
+            <b-form-invalid-feedback>
+              Username is required
+            </b-form-invalid-feedback>
+          </b-form-group>
 
-        <b-form-group
-          id="input-group-Password"
-          label-cols-sm="3"
-          label="Password:"
-          label-for="Password"
-        >
-          <b-form-input
-            id="Password"
-            type="password"
-            v-model="$v.form.password.$model"
-            :state="validateState('password')"
-          ></b-form-input>
-          <b-form-invalid-feedback>
-            Password is required
-          </b-form-invalid-feedback>
-        </b-form-group>
+          <b-form-group
+            id="input-group-Password"
+            label-cols-sm="3"
+            label="Password:"
+            label-for="Password"
+          >
+            <b-form-input
+              id="Password"
+              type="password"
+              v-model="$v.form.password.$model"
+              :state="validateState('password')"
+            ></b-form-input>
+            <b-form-invalid-feedback>
+              Password is required
+            </b-form-invalid-feedback>
+          </b-form-group>
 
-        <b-button
-          type="submit"
-          variant="primary"
-          style="width:100px;display:block;"
-          class="mx-auto w-100"
-          >Login</b-button
+          <b-button
+            type="submit"
+            variant="primary"
+            style="width:100px;display:block;"
+            class="mx-auto w-100"
+          >Login</b-button>
+          <div class="mt-2">
+            Do not have an account yet?
+            <router-link to="register"> Register here</router-link>
+          </div>
+        </b-form>
+        <b-alert
+          class="mt-2"
+          v-if="form.submitError"
+          variant="warning"
+          dismissible
+          show
         >
-        <div class="mt-2">
-          Do not have an account yet?
-          <router-link to="register"> Register in here</router-link>
-        </div>
-      </b-form>
-      <b-alert
-        class="mt-2"
-        v-if="form.submitError"
-        variant="warning"
-        dismissible
-        show
-      >
-        Login failed: {{ form.submitError }}
-      </b-alert>
+          Login failed: {{ form.submitError }}
+        </b-alert>
+      </div>
     </div>
   </div>
 </template>
@@ -65,6 +67,7 @@
 <script>
 import { required } from "vuelidate/lib/validators";
 import { mockLogin } from "../services/auth.js";
+
 export default {
   name: "Login",
   data() {
@@ -94,11 +97,7 @@ export default {
     async Login() {
       try {
         const success = true; // modify this to test the error handling
-        const response = mockLogin(
-          this.form.username,
-          this.form.password,
-          success
-        );
+        const response = mockLogin(this.form.username, this.form.password, success);
 
         console.log(this.$root.store.login);
         this.$root.store.login(this.form.username);
@@ -108,7 +107,6 @@ export default {
         this.form.submitError = err.response.data.message;
       }
     },
-
     onLogin() {
       this.form.submitError = undefined;
       this.$v.form.$touch();
@@ -123,19 +121,27 @@ export default {
 
 <style lang="scss" scoped>
 .login-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
   min-height: 100vh;
-  background: url("/src/assets/food-background.jpg") no-repeat center center;
+}
+
+.image-section {
+  background: url('@/assets/food-background.jpg') no-repeat center center;
   background-size: cover;
 }
 
+.login-form-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
 .login-form {
-  background-color: rgba(255, 255, 255, 0.9);
+  background-color: rgba(255, 255, 255, 0.991);
   padding: 20px;
   border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   max-width: 400px;
   width: 100%;
 }
